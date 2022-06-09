a3be084791faeaf573d7e9024da754d6e063a2be
Eden Kricheli <100904506+Kricheli@users.noreply.github.com>
2022-06-09 12:10:31 +0300

    update

--- src/pages/HomeVideo/VideoPreview.js
+++ src/pages/HomeVideo/VideoPreview.js
@@ -1,34 +1,34 @@
 import React, { useState } from "react";
-import Modal from 'react-bootstrap/Modal';
-export default function ImagePreview({ videoPreview, videoPlay, title, description,vid}) {
+import Modal from "react-bootstrap/Modal";
+export default function ImagePreview({
+  videoPreview,
+  videoPlay,
+  title,
+  description,
+  vid,
+}) {
   const [show, setShow] = useState(false);
   return (
     <>
-    <div className="responsive" >  
-    <div className="galleryVideo">       
-          <img src={videoPreview} className="Img_Grid"
-          onClick={() => setShow(true)}/>
-          <div className="desc">{title}</div> 
+      <div className="responsive">
+        <div className="galleryVideo">
+          <img
+            src={videoPreview}
+            key={title}
+            className="Img_Grid"
+            onClick={() => setShow(true)}
+          />
+          <div className="desc">{title}</div>
           <div>{vid}</div>
-    </div>
-    </div>
-    <Modal
-        show={show}
-        onHide={() => setShow(false)}
-        className="Modal"
-       >
-            <video 
-            src= {videoPlay}
-            width='100%'
-            height='100%'
-            controls
-            playing
-            />
-          <div className="container-Modal">
+        </div>
+      </div>
+      <Modal show={show} onHide={() => setShow(false)} className="Modal">
+        <video src={videoPlay} width="100%" height="100%" controls playing />
+        <div className="container-Modal">
           <h1 className="Title_Modal">{title}</h1>
           <p className="description_Modal">{description}</p>
-          </div>
+        </div>
       </Modal>
-  </>
+    </>
   );
 }
