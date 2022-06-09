492733bb5e22bbf433c5e84e9e3e3a1d10c5a4c1
Eden Kricheli <100904506+Kricheli@users.noreply.github.com>
2022-06-09 12:10:00 +0300

    update

--- src/pages/HomePage/ImagePreview.js
+++ src/pages/HomePage/ImagePreview.js
@@ -1,19 +1,18 @@
-import React, {useState, useEffect} from "react";
+import React, { useState } from "react";
 
-export default function ImagePreview({nasaPicture, title, description}){
-  const [show, setShow]=useState(false);
-  return(
+export default function ImagePreview({ nasaPicture, title, description }) {
+  const [show, setShow] = useState(false);
+  return (
     <div>
-      <img src={nasaPicture} alt={description} onClick={()=>setShow(true)}/>
+      <img src={nasaPicture} alt={description} onClick={() => setShow(true)} />
       <div className="description">{title}</div>
-      <div className="modal" onHide={()=>setShow(false)} show={show}>
-        <img src={nasaPicture} alt={description}/>
+      <div className="modal" onHide={() => setShow(false)} show={show}>
+        <img src={nasaPicture} alt={description} />
         <div>
           <h1>{title}</h1>
           <p>{description}</p>
         </div>
       </div>
     </div>
-
-  )
+  );
 }
