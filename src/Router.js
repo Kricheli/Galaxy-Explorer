139025010b978acaafae8f7a31617d847f61c138
Kricheli <krichelieden@gmail.com>
2023-06-21 21:31:27 +0300

    updating the file source

--- src/Router.js
+++ src/Router.js
@@ -1,19 +1,17 @@
-import React from "react";
-import { BrowserRouter, Route } from "../../node_modules/react-router-dom";
-import HomePage from "../pages/HomePage/HomePage";
-import "../App.css";
-import dailyImage from "../pages/dailyImage/dailyImage";
-import HomeVideo from "../pages/HomeVideo/HomeVideo";
+import React from 'react'
+import { BrowserRouter, Route } from '../node_modules/react-router-dom'
+import HomePage from '../pages/HomePage/HomePage'
+import '../App.css'
+import dailyImage from '../pages/dailyImage/dailyImage'
+import HomeVideo from '../pages/HomeVideo/HomeVideo'
 // require('dotenv').config()
 
-
 export const Router = () => {
-
   return (
     <BrowserRouter>
-          <Route component={HomePage} path="/" exact />
-          <Route component={dailyImage} path="/dailyImage" />
-          <Route component={HomeVideo} path="/HomeVideo" />
+      <Route component={HomePage} path='/' exact />
+      <Route component={dailyImage} path='/dailyImage' />
+      <Route component={HomeVideo} path='/HomeVideo' />
     </BrowserRouter>
-  );
-};
+  )
+}
